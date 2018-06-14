1c481c66a51565310d097ca328c248a4cbf3667d
Anoush <the.a.cars@gmail.com>
2018-06-14 18:25:48 -0400

    convert start time and date to seconds since unix epoch

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import digital_rf as drf
 import sys
 import os
+import datetime
 
 #get filename from command line argument
 path =  os.path.splitext(sys.argv[1])[0]
@@ -11,8 +12,12 @@ UTdate = filename.split("-")[0]
 UTstart = filename.split("-")[1]
 UTend = filename.split("-")[2]
 
-print(UTdate)
-print(UTstart)
-print(UTend)
+#convert human readable start date and time into seconds since UNIX epoch
+y = int(datetime.datetime.strptime(str(UTdate), '%Y%m%d').strftime('%Y'))
+m = int(datetime.datetime.strptime(str(UTdate), '%Y%m%d').strftime('%m'))
+d = int(datetime.datetime.strptime(str(UTdate), '%Y%m%d').strftime('%d'))
+h = int(datetime.datetime.strptime(str(UTstart), '%H%M').strftime('%H'))
+M = int(datetime.datetime.strptime(str(UTstart), '%H%M').strftime('%M'))
 
-#convert human readable start date and time into second since UNIX epoch
+sec_since_epoch = int((datetime.datetime(y,m,d,h,M) - datetime.datetime(1970,1,1)).total_seconds())
+print(sec_since_epoch)
