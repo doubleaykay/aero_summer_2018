4846b1be4361d0d8801fd87af86d00edde1c97a2
Anoush <the.a.cars@gmail.com>
2018-06-14 16:29:31 -0400

    parse date and start/end time from filename

--- convert.py
+++ convert.py
@@ -2,6 +2,14 @@ import digital_rf
 import sys
 import os
 
-path = sys.argv[1] #variable path = path from command line argument
-filename = os.path.splitext(path)[0]
-#print (filename) #uncomment to debug if filename is correct
+path =  os.path.splitext(sys.argv[1])[0] #path with no extension from command line argument
+filename = path.split("/")[4]
+#print(filename) #debug if filename is correct
+
+date = filename.split("-")[0]
+UTstart = filename.split("-")[1]
+UTend = filename.split("-")[2]
+
+print(date)
+print(UTstart)
+print(UTend)
