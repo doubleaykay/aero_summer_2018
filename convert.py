e7e245116dc434330ae0e3f2c64ed2564fd2a646
Anoush <the.a.cars@gmail.com>
2018-06-14 16:07:25 -0400

    pull filename from command line argument

--- convert.py
+++ convert.py
@@ -1,1 +1,7 @@
 import digital_rf
+import sys
+import os
+
+path = sys.argv[1] #variable path = path from command line argument
+filename = os.path.splitext(path)[0]
+#print (filename) #uncomment to debug if filename is correct
