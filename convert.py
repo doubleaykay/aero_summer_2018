6369a4f54bb7877edc9b8b1a0e07819da0415643
Anoush <the.a.cars@gmail.com>
2018-06-14 16:49:26 -0400

    clean up comments

--- convert.py
+++ convert.py
@@ -2,10 +2,17 @@ import digital_rf as drf
 import sys
 import os
 
-path =  os.path.splitext(sys.argv[1])[0] #path with no extension from command line argument
+#get filename from command line argument
+path =  os.path.splitext(sys.argv[1])[0]
 filename = path.split("/")[4]
-#print(filename) #debug if filename is correct
 
-date = filename.split("-")[0]
+#get date and start/end times in human readable format from filename
+UTdate = filename.split("-")[0]
 UTstart = filename.split("-")[1]
 UTend = filename.split("-")[2]
+
+print(UTdate)
+print(UTstart)
+print(UTend)
+
+#convert human readable start date and time into second since UNIX epoch
