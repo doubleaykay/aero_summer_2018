08136c07a15d063727cbf163390b29e8e864f4d2
Anoush <the.a.cars@gmail.com>
2018-06-18 14:06:05 -0400

    get filename from command line argument

--- read.py
+++ read.py
@@ -1,7 +1,9 @@
 import numpy as np
+import os
+import sys
 
-#filename TODO pull from command line arg
-file = "/media/anoush/DEB0D65EB0D63D29/_AERO/working/20180506-0823-0840-TLK-INT.dat"
+#filename from command line argument
+file = sys.argv[1]
 
 #define data type (u2 = 2byte (16bit) unsigned integers)
 type = np.dtype('u2')
