fc6cbfdc3fa77a625819151c719773c2221839a5
Anoush <the.a.cars@gmail.com>
2018-06-18 14:07:23 -0400

    clean comments

--- read.py
+++ read.py
@@ -2,10 +2,10 @@ import numpy as np
 import os
 import sys
 
+#parameters
 #filename from command line argument
 file = sys.argv[1]
-
-#define data type (u2 = 2byte (16bit) unsigned integers)
+#define data type (u2 = 2byte (16bit) unsigned integer)
 type = np.dtype('u2')
 
 #read data
