1452c113bd76e1fb4e0041c7c0fe791e01231b8e
Anoush <the.a.cars@gmail.com>
2018-06-19 16:10:10 -0400

    write data with DigitalRFWriter class

--- read.py
+++ read.py
@@ -1,6 +1,9 @@
+import digital_rf as drf
 import numpy as np
 import os
 import sys
+
+samples_since_epoch = 15255949800000000 #need to get this from the code in metadata.py
 
 #parameters
 #filename from command line argument
@@ -14,4 +17,13 @@ chunks = input("What chunk size do you want to read at a time? ")
 
 #read data
 data = np.fromfile(file, type, count=chunks)
-print(data[antenna])
+#print(data[antenna])
+
+writer = drf.DigitalRFWriter(
+    '/media/anoush/DEB0D65EB0D63D29/_AERO/DRF_TEST_5', dtype=np.dtype('u2'), subdir_cadence_secs=3600, file_cadence_millisecs=1000,
+    start_global_index=samples_since_epoch, sample_rate_numerator=10000000,
+    sample_rate_denominator=1, is_complex=False
+)
+
+writer.rf_write(data[antenna])
+writer.close()
