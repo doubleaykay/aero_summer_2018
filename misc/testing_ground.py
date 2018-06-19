45ca4293cee4ad175f79f5056ffe21f86a23a092
Anoush <the.a.cars@gmail.com>
2018-06-19 18:18:05 -0400

    test reading file in chunks

--- misc/testing_ground.py
+++ misc/testing_ground.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+
 
 # test = [1, 2, 3]
 # print(np.array(test))
@@ -19,10 +21,25 @@ import numpy as np
 #
 # function(1)
 
-antenna = (input("What antenna do you want to read from? ") - 1)
-if(antenna >= 5):
-    print("There are only 4 antennas. Please enter a number from 1 to 4.")
-    antenna = (input("What antenna do you want to read from? ") - 1)
-    print(antenna)
-else:
-    print(antenna)
+# antenna = (input("What antenna do you want to read from? ") - 1)
+# if(antenna >= 5):
+#     print("There are only 4 antennas. Please enter a number from 1 to 4.")
+#     antenna = (input("What antenna do you want to read from? ") - 1)
+#     print(antenna)
+# else:
+#     print(antenna)
+
+file = sys.argv[1]
+type = np.dtype('u2, u2, u2, u2')
+
+def read_in_chunks(file_object, chunk_size=1024):
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+f = open(file, 'r')
+for piece in read_in_chunks(f):
+    data = np.frombuffer(piece, type, count=-1)
+    print(data)
