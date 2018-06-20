bcbe4d1858d6fb271440f0a68f44a060b8ee175e
Anoush <the.a.cars@gmail.com>
2018-06-20 12:57:19 -0400

    use argparse for better command line interface

--- convert.py
+++ convert.py
@@ -3,14 +3,22 @@ import numpy as np
 import os
 import sys
 import datetime
+import argparse
+
+#get arguments from command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="location of file to read from")
+parser.add_argument("-o", "--output", help="location of directory to output to")
+parser.add_argument("-a", "--antenna", help="antenna number to read (1-4)")
+args = parser.parse_args()
 
 #parameters
-#path or filename from command line argument
-file = sys.argv[1]
+#path or filename from argparse
+file = args.input
 #define data type (u2 = 2byte (16bit) unsigned integer)
 type = np.dtype('u2, u2, u2, u2')
-#prompt user for the antenna they wish to read
-antenna = "f" + str((input("What antenna (1-4) to read from? ") - 1))
+#antenna number from argparse
+antenna = "f" + str((int(args.antenna) - 1))
 #prompt user for the number of chunks they want to read at a time
 #chunks = input("What chunk size do you want to read at a time? ")
 #get filename
@@ -44,7 +52,7 @@ f = open(file, 'r')
 
 #create digital_rf writer object
 writer = drf.DigitalRFWriter(
-    '/media/anoush/DEB0D65EB0D63D29/_AERO/DRF_TEST', dtype=np.dtype('u2'),
+    args.output, dtype=np.dtype('u2'),
     subdir_cadence_secs=3600, file_cadence_millisecs=1000,
     start_global_index=samples_since_epoch, sample_rate_numerator=10000000,
     sample_rate_denominator=1, is_complex=False
