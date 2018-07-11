f1b645a01d07c94773da7da74f1d4fd8e571bfc6
Anoush <the.a.cars@gmail.com>
2018-07-11 15:53:07 -0400

    add function to get seconds since epoch from filename

--- labelle_module/labelle.py
+++ labelle_module/labelle.py
@@ -1,10 +1,20 @@
 import os
+import datetime
+import dateutil.parser
 
 """Python module for working with LaBelle group data files"""
 
-def get_filename(n):
+def get_filename(path):
     """Parse provided path to get filename. Provide path as string argument."""
-    file = n
+    file = path
     path =  os.path.splitext(file)[0]
     filename = path.split("/")[path.count('/')]
     return filename
+
+def get_epoch(filename):
+    """Get seconds since epoch from provided filename, assuming standard LaBelle
+    file naming scheme. Provide filename (not path) as string."""
+    UTstart = dateutil.parser.parse(filename.split("-")[0] + filename.split("-")[1])
+    epoch = dateutil.parser.parse('1970, 1, 1')
+    sec_since_epoch = int((UTstart - epoch).total_seconds())
+    return sec_since_epoch
