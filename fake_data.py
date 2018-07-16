39f1bf0ab871c3fa696af0b16987667ca1fce4a6
Anoush <the.a.cars@gmail.com>
2018-07-16 15:11:29 -0400

    get a digital_rf reader

--- fake_data.py
+++ fake_data.py
@@ -2,3 +2,9 @@ import digital_rf as drf
 import scipy
 from scipy import constants
 import numpy as np
+
+#path to existing digital_rf data
+path_to_data = "/media/anoush/5358359c-0dd3-4f0f-89a8-9716f7c6869d/_data/2_converted/20180506-0823-0840-TLK-INT/"
+
+#initialize digital_rf reader
+reader = drf.DigitalRFReader(path_to_data)
