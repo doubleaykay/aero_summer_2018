b3628aeaf726c6af70b82c4c6749cd415734e923
Anoush <the.a.cars@gmail.com>
2018-07-18 12:27:34 -0400

    attempt at using equation 2.2 to calculate a b field from an e field

--- nehori.py
+++ nehori.py
@@ -1,5 +1,22 @@
 import scipy
 from scipy import constants
 import numpy as np
+import math
 
-"""Sample Implementations of Equations from Nehori (1994) paper"""
+"""Sample Implementations of Equations from Nehori (1994)"""
+
+def function_22(fft, freq, type):
+    """Function 2.2 includes a relationship between 'E' and 'H', the vector-phasor representations of E and B fields.
+    This function attempts to leverage that relationship to return a B field computed from the provided FFT of an E field.
+    Pass the unmkodified array output of an FFT, as well as the linear frequency of the data as an integer"""
+
+    direction = [x, y, z] #direction vector from sensor to source of measurement
+    fft_plus = np.split(fft, 2)[0]
+    w = 2 * np.pi * freq
+    e = 2 * fft_plus * math.exp(-w * 1j) #is this the correct way to do multiplicaton of an imaginary number?
+    n = (constants.mu_0 / constants.epsilon_0) ** 0.5
+
+    h_calc = (np.cross(direction, e) / -n)
+    b_calc = np.real(h_calc * math.exp(w * 1j))
+    #b_ifft = np.fft.ifft(b_calc) #do an ifft on the resulting field if need be
+    return b_calc #return fft-like array of b-field
