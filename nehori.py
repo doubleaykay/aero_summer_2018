160ae8f5f3d6e99407f6b9f00776a8c08cb944dd
Anoush <the.a.cars@gmail.com>
2018-07-18 12:31:34 -0400

    clean up and add comments

--- nehori.py
+++ nehori.py
@@ -5,18 +5,18 @@ import math
 
 """Sample Implementations of Equations from Nehori (1994)"""
 
-def function_22(fft, freq, type):
-    """Function 2.2 includes a relationship between 'E' and 'H', the vector-phasor representations of E and B fields.
+def function_22(fft, freq):
+    """Equations 2.2 include a relationship between 'E' and 'H', the 'vector-phasor' representations of E and B fields.
     This function attempts to leverage that relationship to return a B field computed from the provided FFT of an E field.
-    Pass the unmkodified array output of an FFT, as well as the linear frequency of the data as an integer"""
+    Pass in the unmkodified array FFT output, as well as the linear frequency of the data as an integer"""
 
-    direction = [x, y, z] #direction vector from sensor to source of measurement
-    fft_plus = np.split(fft, 2)[0]
-    w = 2 * np.pi * freq
+    direction = [x, y, z] #PLACEHOLDER NEEDS TO BE CORRECT AND PASSED IN direction vector from sensor to source of measurement
+    fft_plus = np.split(fft, 2)[0] #get the positive frequency component
+    w = 2 * np.pi * freq #angular frequency from linear frequency of e field
     e = 2 * fft_plus * math.exp(-w * 1j) #is this the correct way to do multiplicaton of an imaginary number?
-    n = (constants.mu_0 / constants.epsilon_0) ** 0.5
+    n = (constants.mu_0 / constants.epsilon_0) ** 0.5 #intrinsic impedance (mu and epsilon of material should be passed in)
 
-    h_calc = (np.cross(direction, e) / -n)
-    b_calc = np.real(h_calc * math.exp(w * 1j))
+    h_calc = (np.cross(direction, e) / -n) #euqation solved for H
+    b_calc = np.real(h_calc * math.exp(w * 1j)) #B pulled from H
     #b_ifft = np.fft.ifft(b_calc) #do an ifft on the resulting field if need be
     return b_calc #return fft-like array of b-field
