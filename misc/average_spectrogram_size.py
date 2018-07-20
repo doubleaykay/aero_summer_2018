b6a0f8d8f598f1efb54ac3e50df56ddab39bfb9a
Anoush <the.a.cars@gmail.com>
2018-07-20 17:16:16 -0400

    define functions

---
+++ misc/average_spectrogram_size.py
@@ -0,0 +1,7 @@
+import os
+
+def list_files_in_dir(root_dir):
+    """Return a list of all file paths within a directory, including files in subdirectories."""
+
+def average_file_size(file_list):
+    """Return average file size in bytes of numerous files. Pass in file paths as a list."""
