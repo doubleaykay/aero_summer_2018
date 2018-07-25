3279760131977b17dd21fa338f91d9d2384a867a
Anoush <the.a.cars@gmail.com>
2018-07-24 21:28:40 -0400

    simplify code

--- binary_compression/binary_compression_tools.py
+++ binary_compression/binary_compression_tools.py
@@ -3,15 +3,11 @@ import numpy as np
 def read_txt_into_array(path):
     """Return data as a numpy array."""
     f = open(path, 'r')
-    temp1 = []
-    temp2 = []
+    temp = []
     for line in f:
-        temp1.append(line)
-    for b in temp1:
-        b.replace('\n', '')
-        temp2.append(float(b))
+        temp.append(float(line.replace('\n', '')))
+    data = np.float64(np.array(temp))
     temp = None
-    data = np.float64(np.array(temp2))
     return data
 
 #begin test program
