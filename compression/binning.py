2a6e28e1b39cc84e823f81ac3646f7ca5c39a827
Anoush <the.a.cars@gmail.com>
2018-07-31 12:09:20 -0400

    use correct terminology (byte not bit)

--- compression/binning.py
+++ compression/binning.py
@@ -1,27 +1,27 @@
 import numpy as np
 import pickle
 
-"""Functions and code to bin data into bits and nibbles."""
+"""Functions and code to bin data into bytes and nibbles."""
 
-def bin_to_bits(raw, low, high):
-    """Bin data into bits.
+def bin_to_bytes(raw, low, high):
+    """Bin data into bytes.
     :raw: data array
     :low: int, minimum value of data
     :high: int, maximum value of data
     Returns a numpy array with binned data."""
 
-    bit = np.linspace(low, high, 256)
-    data_bit = []
+    byte = np.linspace(low, high, 256)
+    data_byte = []
     for b in raw:
         i = 0
         while i <= 254:
-            if (bit[i] <= b < bit[i+1]):
-                data_bit.append(i)
+            if (byte[i] <= b < byte[i+1]):
+                data_byte.append(i)
                 i += 1
                 break
             else:
                 i += 1
-    return np.array(data_bit)
+    return np.array(data_byte)
 
 def bin_to_nibble(raw, low, high):
     """Bin data into nibbles.
