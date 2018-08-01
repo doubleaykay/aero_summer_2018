a9666e371625392df6dd75161f4b8939de90b148
Anoush <the.a.cars@gmail.com>
2018-08-01 14:03:23 -0400

    function for custom bit depth binning

--- compression/binning.py
+++ compression/binning.py
@@ -55,6 +55,34 @@ def bin_to_nibble(raw, low, high):
                 i += 1
     return np.array(data_nibble)
 
+def bin(raw, depth, low, high):
+    """Bin data into specified bit-depth.
+    :raw: data array
+    :depth: int, bit depth (i.e. 8, 4, etc)
+    :low: int, minimum value of data
+    :high: int, maximum value of data
+    Returns a numpy array with binned data."""
+
+    max_in_depth = 2 ** depth
+    bin_range = np.linspace(low, high, max_in_depth)
+    data = []
+    for b in raw:
+        i = 0
+        while i <= (max_in_depth - 2):
+            if (bin_range[i] <= b < bin_range[i+1]):
+                data.append(i)
+                i += 1
+                break
+            elif (b <= low):
+                data.append(0)
+                break
+            elif (b >= high):
+                data.append(max_in_depth)
+                break
+            else:
+                i += 1
+    return np.array(data)
+
 # Begin program
 
 # IO variables
