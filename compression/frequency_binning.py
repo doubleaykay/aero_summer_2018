d2edae07f646220d340154482ff29642a1060160
Anoush <the.a.cars@gmail.com>
2018-08-07 17:21:18 -0400

    define binning function for modularity

--- compression/frequency_binning.py
+++ compression/frequency_binning.py
@@ -1,11 +1,16 @@
 import numpy as np
 
+def bin_freq(array, max_freq):
+    # calculate bin cadence
+    bin_cadence = round((float(max_freq) / len(array) * 1000), 2)
+
+
+
+
 # IO variables
 psd_txt = '/home/anoush/Desktop/working/freq_binning/20170917-0929-0934-TLK-INT/ant1/raw/psd.txt'
 
 # load data from psd_txt
 data = np.loadtxt(psd_txt)
 
-# calculate bin cadence
-max_freq = 5000
-bin_cadence = round((float(max_freq) / len(data) * 1000), 2)
+bin_freq(data, 5000)
