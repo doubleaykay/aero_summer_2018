ac7642dd6d001234184733e4f518109f17386373
Anoush <the.a.cars@gmail.com>
2018-08-07 18:08:12 -0400

    add function docstring

--- compression/frequency_binning.py
+++ compression/frequency_binning.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def bin_freq(array, max_freq):
+def bin_freq_avg10(array, max_freq):
+    """Bin by frequencies using an average by 10 binning scheme. Returns numpy array of binned values.
+    :array: raw data
+    :max_freq: int, maximum frequency in data"""
+
     # calculate bin cadence
     bin_cadence = round((float(max_freq) / len(array) * 1000), 2)
 
@@ -13,4 +17,4 @@ psd_txt = '/home/anoush/Desktop/working/freq_binning/20170917-0929-0934-TLK-INT/
 # load data from psd_txt
 data = np.loadtxt(psd_txt)
 
-bin_freq(data, 5000)
+bin_freq_avg10(data, 5000)
