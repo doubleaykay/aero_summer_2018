57f1058e53ea21cf401e6d89f48d0527da878eda
Anoush <the.a.cars@gmail.com>
2018-08-09 17:39:43 -0400

    add alternate function that expands values to be readily plotted

--- compression/frequency_binning2.py
+++ compression/frequency_binning2.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def avg10(array, bins, num_fft, max_freq):
-    """Return a frequency binned numpy array of data. Frequency ranges to compress are hard coded,
+    """Return a frequency binned numpy array of data. Does not expand average bins,
+    so there is a smaller number of bins than before. Frequency ranges to compress are hard coded,
     and compression is done via a factor-of-10 average.
 
     :array: numpy array, raw data
@@ -59,6 +60,81 @@ def avg10(array, bins, num_fft, max_freq):
             compressed.append(np.average(working[b:c]))
             b += 10
         del b, c
+
+        del working
+        a += 1
+
+    return np.array(compressed)
+
+def avg10_plot(array, bins, num_fft, max_freq):
+    """Return a frequency binned numpy array of data. Expands bins so that there are
+    the same number of bins as before. Output can be plotted with drf_sti.
+    Frequency ranges to compress are hard coded, and compression is done via a factor-of-10 average.
+
+    :array: numpy array, raw data
+    :bins: int, number of time bins
+    :num_fft: int, number of FFT frequency bins
+    :max_freq: int, maximum frequency of data in Hz"""
+
+    if not len(array) == (bins * num_fft):
+        raise ValueError('Incorrect number of time or frequency bins provided.')
+
+    # reshape data array into bins
+    # now, the dimentions are: raw[spectra (time bin) number,frequency (FFT bin) number]
+    raw = array.reshape((-1,num_fft))
+
+    # initialize empty list to append compressed values to
+    compressed = []
+
+    # calculate FFT bin frequency step
+    step = round((float(max_freq) / num_fft), 2)
+
+    a = 0
+    while a <= (raw.shape[0] - 1):
+        working = raw[a,...]
+
+        # average the first 20 bins
+        b = 0
+        while b <= 10:
+            c = b + 10
+            i = 0
+            while i <= 9:
+                compressed.append(np.average(working[b:c]))
+                i += 1
+            b += 10
+        del b, c, i
+
+        # do not average the next 103 bins
+        for d in working[20:123]:
+            compressed.append(d)
+        del d
+
+        # average the next 410 bins
+        b = 123
+        while b <= 523:
+            c = b + 10
+            i = 0
+            while i <= 9:
+                compressed.append(np.average(working[b:c]))
+                i += 1
+            b += 10
+        del b, c, i
+
+        # do not average the next 101 bins
+        for d in working[533:634]:
+            compressed.append(d)
+        del d
+
+        # average the next 390 bins
+        b = 634
+        while b <= 1014:
+            c = b + 10
+            i = 0
+            while i <= 9:
+                compressed.append(np.average(working[b:c]))
+                i += 1
+            b += 10
+        del b, c, i
 
         del working
         a += 1
