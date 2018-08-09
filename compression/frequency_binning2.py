96007a173efe080648ac3eeb27ef84ef289c2dff
Anoush <the.a.cars@gmail.com>
2018-08-09 15:38:05 -0400

    initialize compressed as list instead, to avoid defining shape

--- compression/frequency_binning2.py
+++ compression/frequency_binning2.py
@@ -8,18 +8,18 @@ def avg10(array, bins, num_fft, max_freq):
     # now, the dimentions are: raw[spectra (time bin) number,frequency (FFT bin) number]
     raw = array.reshape((-1,num_fft))
 
-    compressed = np.array()
+    compressed = []
 
     # calculate FFT bin frequency step
     step = round((float(max_freq) / num_fft), 2)
-    
+
     a = 0
     while a <= (raw.shape[0] - 1):
         working = raw[a,...]
 
         a += 1
 
-    return compressed
+    return np.array(compressed)
 
 # IO variables
 psd_txt = '/home/anoush/Desktop/working/freq_binning/20170917-0929-0934-TLK-INT/ant1/raw/psd.txt'
