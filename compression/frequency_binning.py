20c0ecd80fbff05313400a986c5c66184900dd75
Anoush <the.a.cars@gmail.com>
2018-08-13 08:32:33 -0400

    test code for generalized frequency binning function

--- compression/frequency_binning.py
+++ compression/frequency_binning.py
@@ -1,5 +1,36 @@
 import numpy as np
 
+def freq_binning(array, factor, expand=False):
+    # check that array length is a factor of the factor provided
+    if not (len(array) % factor) == 0:
+        raise ValueError('Length of array is not a multiple of ' + str(factor) + '!')
+
+    # bin by frequency
+    compressed = []
+    a = 0
+    while a <= (len(array) - factor):
+        b = (a + factor)
+        if expand:
+            i = 1
+            while i <= factor:
+                compressed.append(np.average(array[a:b]))
+                i += 1
+        if not expand:
+            compressed.append(np.average(array[a:b]))
+        a += factor
+
+    # check new length as opposed to original array; return array if okay to do so
+    if expand:
+        if len(compressed) == len(array):
+            return compressed
+        else:
+            raise RuntimeError('Compression did not work, as new list is not as long as original list.')
+    if not expand:
+        if float(len(array)) / float(len(compressed)) == factor:
+            return compressed
+        else:
+            raise RuntimeError('Compression did not work, as new and old list lengths are not related by given factor.')
+
 def avg10(array, bins, num_fft, max_freq):
     """Return a frequency binned numpy array of data. Does not expand average bins,
     so there is a smaller number of bins than before. Frequency ranges to compress are hard coded,
