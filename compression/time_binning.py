111dba215a7f7353ef1984ff140881ca5b120cc9
Anoush <the.a.cars@gmail.com>
2018-08-15 15:32:01 -0400

    begin working on time binning

---
+++ compression/time_binning.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def time_binning(array, factor, expand=False):
+    # check that array length is a factor of the factor provided
+    if not (len(array) % factor) == 0:
+        raise ValueError('Length of array is not a multiple of ' + str(factor) + '!')
