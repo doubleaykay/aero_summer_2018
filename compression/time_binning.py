f2e292bb73640c9db2ff1ae0c43674c984afab03
Anoush <the.a.cars@gmail.com>
2018-08-15 16:23:09 -0400

    add the rest of the binning function

--- compression/time_binning.py
+++ compression/time_binning.py
@@ -1,6 +1,34 @@
 import numpy as np
 
+# this following function is the same one used in frequency binning. I should make this into an even futher generalized function...
+
 def time_binning(array, factor, expand=False):
     # check that array length is a factor of the factor provided
     if not (len(array) % factor) == 0:
         raise ValueError('Length of array is not a multiple of ' + str(factor) + '!')
+
+    # bin by time
+    compressed = []
+    a = 0
+    while a <= (len(array) - factor):
+        b = (a + factor)
+        if expand:
+            i = 1
+            while i <= factor:
+                compressed.append(np.average(array[a:b]))
+                i += 1
+        if not expand:
+            compressed.append(np.average(array[a:b]))
+        a += factor
+
+    # check new length as opposed to original array; return array if okay to do so
+    if expand:
+        if len(compressed) == len(array):
+            return compressed
+        else:
+            raise RuntimeError('Compression did not work, as new list is not as long as original list.')
+    if not expand:
+        if float(len(array)) / float(len(compressed)) == factor:
+            return compressed
+        else:
+            raise RuntimeError('Compression did not work, as new and old list lengths are not related by given factor.')
