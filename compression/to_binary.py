1fe6cef77949ab6fcb36facb734b529a3404efaa
Anoush <the.a.cars@gmail.com>
2018-08-15 13:35:56 -0400

    read function (thanks ryan)

--- compression/to_binary.py
+++ compression/to_binary.py
@@ -18,3 +18,11 @@ def as_numpy(filename):
     f.close()
 
     print('Success.')
+
+def read(filename):
+    a = np.fromfile(filename, dtype='i1')
+    b0 = np.bitwise_and(a,15)
+    b1 = np.bitwise_and(a,15<<4) >> 4
+    c = np.stack((b0,b1))
+
+    return c.T.reshape(-1)
