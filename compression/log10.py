339e8da876876dea165baf7ebcbd6f3e857d7c02
Anoush <the.a.cars@gmail.com>
2018-08-21 12:52:53 -0400

    log10 code to pass file and save log10 file

--- compression/log10.py
+++ compression/log10.py
@@ -1,4 +1,16 @@
 import numpy as np
+import argparse
 
 def log10(array):
     return np.log10(array)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="location of file to read from")
+args = parser.parse_args()
+
+in_file = args.input
+out_file = in_file.split('.')[0] + '_log10.txt'
+
+f = open(out_file, 'w+')
+log10(np.loadtxt(in_file)).tofile(f, '\n')
+f.close()
