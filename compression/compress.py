5f842a21c763f6e73871e47ace559d7318052509
Anoush <the.a.cars@gmail.com>
2018-11-28 17:20:14 -0500

    file for complete compression script

---
+++ compression/compress.py
@@ -0,0 +1,9 @@
+# read drf
+# convert to spectral data (sti_intermediate_file.py)
+# store freq and sti_times array as binary using write_bin() for plotting tool
+# take psd array to be processed
+# apply frequency binning scheme 4
+# apply time binning scheme 2
+# take log 10
+# apply 4-bit amplitude binning
+# store as binary
