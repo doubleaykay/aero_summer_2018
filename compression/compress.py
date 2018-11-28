13e4d490af3072ee0503697888adc0b8ca594608
Anoush <the.a.cars@gmail.com>
2018-11-28 17:25:30 -0500

    parse arguments needed to get spectral data

--- compression/compress.py
+++ compression/compress.py
@@ -11,3 +11,11 @@
 import argparse
 import digital_rf as drf
 import numpy as np
+
+#get arguments from command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="location of drf directory to read from")
+parser.add_argument("-o", "--output", help="location of directory to output to")
+parser.add_argument("-n", "--num_fft", nargs='?', const=2048, type=int, default=2048, help="number of frequency bins (i.e. number of data points in spectra)")
+parser.add_argument("-c", "--channel", help="drf channel to read from")
+parser.add_argument("-b", "--bins", nargs='?', const=1000, type=int, default=1000, help="number of time bins (i.e. number of spectra)")
