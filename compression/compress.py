dc756c688a14471ece7faf2726eff942a63ee821
Anoush <the.a.cars@gmail.com>
2018-11-29 11:21:13 -0500

    time binning function and code (from time_binning)

--- compression/compress.py
+++ compression/compress.py
@@ -78,6 +78,7 @@ def binning(array, factor, expand=False):
         else:
             raise RuntimeError('Compression did not work, as new and old list lengths are not related by given factor.')
 
+# frequency scheme4 function
 def freq_scheme4(array, expand):
     bins = 1000
     num_fft = 1024
@@ -122,6 +123,55 @@ def freq_scheme4(array, expand):
         a += 1
 
     return np.array(compressed)
+
+# time scheme2 function
+def time_scheme2(array):
+    bins = 1000
+    num_fft = 1024
+
+    # reshape into two axis: axis zero is time, axis 1 is frequency
+    # then take the transpose, so that axis zero is frequency
+    raw = array.reshape((-1,num_fft)).T
+
+    # create new array
+    new = np.zeros(raw.shape)
+
+    # don't time average in frequency bins 0 to 124
+    new[0:124,...] = raw[0:124,...]
+
+    # time average in frequency bins 124 to 288
+    factor1 = 5
+    a = 124
+    while a <= 288:
+        # b = a + 1
+        compress = raw[a,...]
+        new[a,...] = binning(compress, factor1, expand=True)
+        a += 1
+    del a
+
+    # don't time average in frequency bins 288 to 862
+    new[288:862,...] = raw[288:862,...]
+
+    # time average in frequency bins 862 to 1024
+    factor2 = 5
+    a = 862
+    while a <= 1023:
+        # b = a + 1
+        compress = raw[a,...]
+        new[a,...] = binning(compress, factor2, expand=True)
+        a += 1
+    del a
+
+    # transpose back to the original array shape
+    new1 = new.T
+
+    # ensure that the array is the correct shape
+    if new1.shape == (bins, num_fft):
+        # make array one dimensional again
+        new2 = new1.reshape((1,-1))
+        return new2
+    else:
+        raise RuntimeError('Output array is the wrong shape, something went wrong.')
 
 # BEGIN PROGRAM
 #get arguments from command line
@@ -224,3 +274,6 @@ write_bin(sti_times).tofile(dir_out + 'sti_times.dat')
 
 # FREQUENCY BINNING (FREQ_SCHEME4)
 psd_freq = freq_scheme4(psd, True)
+
+# TIME BINNING (TIME_SCHEME2)
+psd_time = time_scheme2(psd_freq)
