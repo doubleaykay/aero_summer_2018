aca4eea25891165e95e9fd1afeafff0f2edd48ed
Anoush <the.a.cars@gmail.com>
2018-11-29 11:31:06 -0500

    remove staging comments

--- compression/compress.py
+++ compression/compress.py
@@ -1,13 +1,3 @@
-# read drf
-# convert to spectral data (sti_intermediate_file.py)
-# store freq and sti_times array as binary using write_bin() for plotting tool
-# take psd array to be processed
-# apply frequency binning scheme 4
-# apply time binning scheme 2
-# take log 10
-# apply 4-bit amplitude binning
-# store as binary
-
 import argparse
 import digital_rf as drf
 import numpy as np
